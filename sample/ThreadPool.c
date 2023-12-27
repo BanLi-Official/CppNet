98ec8a04c5d7fe02c9413c3e316d8bef65eebcbb
XieXin <94674279+BanLi-Official@users.noreply.github.com>
2023-12-27 11:02:32 +0800

    2023年12月27日11:01:47
    修改了线程池的c文件

--- sample/ThreadPool.c
+++ sample/ThreadPool.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#define NUMBER 2
 
 // 任务结构体
 typedef struct Task
@@ -103,10 +104,140 @@ ThreadPool* threadPoolCreate(int min,int max,int queueSize)
 
 void* manager(void* arg)
 {
+    //变量初始化
+    ThreadPool *pool=(ThreadPool *)arg;
+    
+    //循环每3秒检测一次（在线程池没有被shutdown的情况下）
+    while (!pool->shutdown)
+    {
+        _sleep(3);
+        //取基础数据
+        pthread_mutex_lock(&pool->mutexPool);
+        int queueSize=pool->queueSize;
+        int LiveNum=pool->liveNum;
+        int BusyNum=pool->busyNum;
+        pthread_mutex_unlock(&pool->mutexPool);
+            
+        //创建线程
+        //任务的个数>存活的线程个数 &&存活的线程<最大的线程数
+        if(queueSize>LiveNum && LiveNum<pool->maxNum)
+        {
+            pthread_mutex_lock(&pool->mutexPool);
+            int count=0;//当次已经创建的线程的数量
+            for(int i=0;i<pool->maxNum && count < NUMBER 
+                && pool->liveNum<pool->maxNum;i++)
+            {
+                if(pool->threadIDs[i]==0)
+                {
+                    pthread_create(&pool->threadIDs[i],NULL,worker,pool);
+                    pool->liveNum++;
+                    count++;
+                }
+            }
+            pthread_mutex_unlock(&pool->mutexPool);
+        }
 
+        //杀死线程
+        //忙的线程*2<存活的线程 && 存活的线程>最小的线程数
+        if(BusyNum*2 < LiveNum && LiveNum > pool->minNum)
+        {
+            pthread_mutex_lock(&pool->mutexPool);
+            pool->exitNum=NUMBER;
+            pthread_mutex_unlock(&pool->mutexPool);
+            //唤醒等待notEmpty资源的线程，并诱导其自杀
+            for(int i=0;i<NUMBER;i++)
+            {
+                pthread_cond_signal(&pool->notEmpty);
+            }
+        }
+        
+    }
+
+    return NULL;
 }
-void* worker(void* arg)
+
+
+
+
+
+void threadExit(ThreadPool *pool)
 {
+    pthread_t pid=pthread_self();
+    for(int i=0;i<pool->maxNum;i++)
+    {
+        if(pool->threadIDs[i]==pid)
+        {
+            pool->threadIDs[i]=0;
+            printf("thread %ld Exits",pid);
+            break;
+        }
+    }
+    pthread_exit(NULL);
+}
+
+
+void *worker(void *arg)
+{
+    ThreadPool *pool=(ThreadPool *)arg;
+    while (1)  //不断从任务队列中取任务并进行运行
+    {
+        //先给线程池上锁
+        pthread_mutex_lock(&pool->mutexPool);
+        //判断当前任务队列是否为空
+        while(pool->queueSize==0&& !pool->shutdown)
+        {//任务队列为空的时候阻塞线程
+            //当线程调用pthread_cond_wait时，它首先会释放mutexPool互斥锁，
+            //   在等待条件变量的过程中，线程会阻塞，直到其他线程调用pthread_cond_signal或pthread_cond_broadcast来
+            //   通知条件变量的状态发生了改变。一旦收到信号，线程会重新获取mutexPool互斥锁，并继续执行后面的代码。
+            pthread_cond_wait(&pool->notEmpty,&pool->mutexPool);
+
+            //判断是不是要销毁线程,这个exitNum来源于管理者的设定，告诉线程们，有多少线程要自杀
+            if(pool->exitNum>0) 
+            {
+                pool->exitNum--;
+                if(pool->liveNum>pool->minNum)
+                {
+                    pool->liveNum--;
+                    pthread_mutex_unlock(&pool->mutexPool);
+                    threadExit(pool);//线程自杀
+                }
+            }
+        }
+
+        //判断线程池是否被关闭了
+        if(pool->shutdown)
+        {
+            pthread_mutex_unlock(&pool->mutexPool);
+            threadExit(pool);
+        }
+
+        //一切正常后，从循环任务队列中拉取任务进行运行
+        Task task;
+        task=pool->taskQ[pool->queueFront];
+
+        //移动头结点
+        pool->queueFront=(pool->queueFront+1)%pool->queueCapacity;
+        pool->queueSize--;
+
+        //发出notFull的信号并给pool解锁
+        pthread_cond_signal(&pool->notFull);
+        pthread_mutex_unlock(&pool->mutexPool);
+
+        printf("thread %ld start working.....",pthread_self());
+        pthread_mutex_lock(&pool->mutexBusy);
+        pool->busyNum++;
+        pthread_mutex_unlock(&pool->mutexBusy);
+        task.function(task.arg);
+        free(task.arg);
+        task.arg=NULL;
+        printf("thread %ld end working.......",pthread_self());
+        pthread_mutex_lock(&pool->mutexBusy);
+        pool->busyNum--;
+        pthread_mutex_unlock(&pool->mutexBusy);
+
+    }
+
+    return NULL;
     
 }
 
