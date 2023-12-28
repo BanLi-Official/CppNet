3bbbf08a420ecb9c771f9ff1b331ee776d95d81d
XieXin <94674279+BanLi-Official@users.noreply.github.com>
2023-12-28 14:23:41 +0800

    2023年12月28日14:23:38

--- threadPool/taskQueue.cpp
+++ threadPool/taskQueue.cpp
@@ -1,3 +1,38 @@
-#include "taskQueue.h"
+#include "taskQueue.hpp"
+#include <iostream>
 
+using namespace std;
 
+int main()
+{
+    cout<<"hello world!";
+    return 0;
+}
+
+taskQ::taskQ()
+{
+    pthread_mutex_init(&this->m_mutex,NULL);
+
+}
+
+taskQ::~taskQ()
+{
+    pthread_mutex_destroy(&this->m_mutex);
+}
+
+void taskQ::inputTask(task a)
+{
+    this->taskQueue.push(a);
+}
+
+void taskQ::inputTask(callback f, void *arg)
+{
+    this->taskQueue.push(task(f,arg));
+}
+
+task taskQ::getTask()
+{
+    task res=this->taskQueue.front();//取最前面第一个任务
+    this->taskQueue.pop();//弹出第一个任务
+    return task();
+}
--- threadPool/taskQueue.hpp
+++ threadPool/taskQueue.hpp
@@ -1,7 +1,8 @@
 #include <queue>
-#include <mingw.thread.h>
+
+#include <pthread.h>
 #include <mutex>
-#include "taskqueue.cpp"
+
 
 using callback =void(*)(void*);
 using namespace std;
@@ -27,10 +28,27 @@ struct task
 class taskQ //任务队列
 {
 public:
+    //构造函数
+    taskQ();
 
+    //析构函数
+    ~taskQ();
+
+    //放入任务
+    void inputTask(task a);
+    void inputTask(callback f,void *arg);
+
+    //取出任务
+    task getTask();
+
+    //获取任务数量
+    inline int getTaskNum()
+    {
+        return taskQueue.size();
+    }
 
 private:
-    mutex m_mutex;
+    pthread_mutex_t m_mutex;
     
     queue<task> taskQueue;
     
